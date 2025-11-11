50941ca38c98b5ab29d1eb5ea9a53053477bf324
Fashion Studio <fashionstudio.bhiwandi@gmail.com>
2025-11-11 09:16:44 +0000

    change the app logo to a sewing machine

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,6 +4,8 @@ export function SirahAndAdnLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -12,12 +14,14 @@ export function SirahAndAdnLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M4 8c1.5-1.5 4-1.5 5.5 0" />
-      <path d="M9.5 16c1.5 1.5 4 1.5 5.5 0" />
-      <path d="M4 16c1.5 1.5 4 1.5 5.5 0" />
-      <path d="M9.5 8c1.5-1.5 4-1.5 5.5 0" />
-      <path d="M14.5 3l-5 5" />
-      <path d="M14.5 13l-5 5" />
+      <path d="M16 19h4" />
+      <path d="M12 19h2" />
+      <path d="M12 5a4 4 0 0 1 4 4v5h-4Z" />
+      <path d="M16 14h2a2 2 0 0 1 2 2v1h-4v-3Z" />
+      <path d="M4.156 8.44A2 2 0 0 0 6 8h3" />
+      <path d="M7 8V5a2 2 0 0 1 2-2h1" />
+      <circle cx="14" cy="9" r="2" />
+      <circle cx="7" cy="15" r="2" />
     </svg>
   );
 }
